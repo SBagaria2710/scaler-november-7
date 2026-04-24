7586638d7abf3be689adac236d20ccaf93d73c44
SBagaria2710 <shashwawtforwork@gmail.com>
2026-04-24 22:48:59 +0530

    New build and some logs

--- class-42-bms/server/server.js
+++ class-42-bms/server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
 
 // Routes
 import userRoutes from "./routes/userRoutes.js";
@@ -13,13 +16,44 @@ import bookingRouter from "./routes/bookingRoutes.js";
 dotenv.config();
 connectDB();
 
+
 const app = express();
+app.use(helmet());
+app.disable("x-powered-by");
+
+// Custom Content Security Policy (CSP) configuration
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "example.com", "scaler.com"], // Allow scripts from 'self' and example.com
+      styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles (unsafe)
+      imgSrc: ["'self'", "data:", "example.com"], // Allow images from 'self', data URLs, and example.com
+      connectSrc: ["'self'", "api.example.com"], // Allow connections to 'self' and api.example.com
+      fontSrc: ["'self'", "fonts.gstatic.com"], // Allow fonts from 'self' and fonts.gstatic.com
+      objectSrc: ["'none'"], // Disallow object, embed, and applet elements
+      upgradeInsecureRequests: [], // Upgrade insecure requests to HTTPS
+    },
+  })
+);
+
+// Sanitize user input to prevent Mongo Operator Injection
+app.use(mongoSanitize());
 
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
 app.use(express.json());
+
+// Rate Limiter Config
+const apiLimiter = rateLimit({
+    windowMs: 15* 60 * 1000, // 15 minutes
+    max: 50, // Limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again after 15 minutes.",
+});
+app.use("/api", apiLimiter);
+
 
 app.use("/api/user", userRoutes);
 app.use("/api/movie", movieRouter);
